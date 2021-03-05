0b7a071c26094fac65c40762c52a1cb0ff9cb065
bilal <bilal.rizwaan1909@gmail.com>
2021-03-05 16:56:07 +0500

    Update Pos Clone Because i test vuex

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import Vuex from 'vuex'
-import store from '@/store'
+import store from './store'
 
 createApp(App)
 .use(Vuex)
