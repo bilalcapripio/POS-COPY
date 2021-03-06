abe3547756f5b14a0eea7f76103030064ec6f6a8
bilal <bilal.rizwaan1909@gmail.com>
2021-03-06 11:46:45 +0500

    Set Layout of Pos Page

--- src/components/Main-Pos/Pos.vue
+++ src/components/Main-Pos/Pos.vue
@@ -118,7 +118,7 @@
 <!--//////////////////----End Navbar----//////////////////-->
         <main>
             <div class="container-fluid mt-3">
-                <div class="row">
+                <div>
                     <PosRight/>
                     <PosLeft/>
                 </div>
