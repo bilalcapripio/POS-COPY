d728626c2699f506a669cf700b44eecdd2634ce5
bilal <bilal.rizwaan1909@gmail.com>
2021-03-09 12:52:16 +0500

    Authentication Page

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -2,36 +2,36 @@
     <div>
         <section>
             <div class="container">
-                    <div class="row justify-content-center align-items-center" style="margin-left:2.3rem;">
+                    <div class="row justify-content-center align-items-center" style="margin-left:8rem;">
                         <div class="w-50">
-                            <div class="d-flex mx-auto p-3">
-                                <img src="../assets/Img/pos.png" width="250" alt="AdminLTE Logo" class="img-fluid">
+                            <div class="d-flex mx-auto p-4">
+                                <img src="../assets/Img/pos.png" width="270" alt="AdminLTE Logo" class="img-fluid mx-4">
                             </div>
                             <!-- form card login -->
                                 <div class="card card-outline-secondary w-75">
-                                    <div class="card-header py-4 text-center">
+                                    <div class="card-header py-3 text-center">
                                         <h4 class="mb-0 lead">Sign in to start your session</h4>
                                     </div>
                                     <div class="card-body p-5">
                                         <form>
                                             <div class="form-group">
-                                                <label for="uname1">Username</label> 
-                                                <input class="form-control"  required type="text">
+                                                <input class="form-control"  required type="text" value="admin">
                                             </div>
 
                                             <div class="form-group">
-                                                <label>Password</label> 
-                                                <input  class="form-control"  required type="password">
+                                                <input  class="form-control"  required type="password" value="admin">
                                             </div>
 
                                             <div class="form-check small">
                                                 <label class="form-check-label">
                                                     <input class="form-check-input" type="checkbox"> 
                                                     <span>Remember me on this computer</span>
+                                                    <br>
+                                                    <span><a href="$">I Forget my password </a></span>
+
                                                 </label>
                                             </div>
-
-                                            <button class="btn btn-success btn-lg float-right">Login</button>
+                                            <button class="btn btn-info float-right">Login</button>
                                         </form>
                                     </div>
                                     <!--/card-block-->
