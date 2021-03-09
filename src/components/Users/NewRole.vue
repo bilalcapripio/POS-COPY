6df32d95fe6981955df5c7d4eeb958cb04175158
bilal <bilal.rizwaan1909@gmail.com>
2021-03-09 19:50:30 +0500

    New Role

--- src/components/Users/NewRole.vue
+++ src/components/Users/NewRole.vue
@@ -11,16 +11,66 @@
                                 <div class="col-md-12">
                                     <div class="card">
                                         <div class="card-header">
-                                            <h5 class="card-header display-5">Please Enter Valid Data</h5>
+                                            <h5 class="card-header lead">Please Enter Valid Data</h5>
                                         </div>
                                         <div class="card-body">
-                                            <form>
-
-                                            </form>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <form class="mx-auto">
+                                                        <div class="form-group row">
+                                                            <label for="staticEmail" class="col-sm-2 col-form-label">Role Name<span class="text-danger">*</span> </label>
+                                                            <div class="col-sm-10">
+                                                            <input type="text"  class="form-control" id="staticEmail">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label for="inputPassword" class="col-sm-2 col-form-label">Description</label>
+                                                            <div class="col-sm-10">
+                                                                <textarea name=""  class="form-control"  id="" cols="30" rows="3"></textarea>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="col-md-6"></div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                            <!-- start table -->
+                            <div class="col-sm-12">
+                                <table class="table table-bordered">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">3</th>
+                                        <td>Larry</td>
+                                        <td>the Bird</td>
+                                        <td>@twitter</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- end table -->
                         </div>
                     </div> 
                     <Footer/>
