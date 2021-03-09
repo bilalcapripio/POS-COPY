d99c8e1cb82437e35a6c9e9fba79807a41d70a99
bilal <bilal.rizwaan1909@gmail.com>
2021-03-09 14:09:15 +0500

    Authentication Page Animation

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -2,14 +2,28 @@
     <div>
         <section>
             <div class="container">
+                    <div class="" >
+                        <ul class="circles">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                        </ul>
+                    </div>
                     <div class="row justify-content-center align-items-center" style="margin-left:8rem;">
                         <div class="w-50">
                             <div class="d-flex mx-auto p-4">
-                                <img src="../assets/Img/pos.png" width="270" alt="AdminLTE Logo" class="img-fluid mx-4">
+                                <img src="../assets/Img/pos.png" width="210" alt="AdminLTE Logo" class="img-fluid mx-5">
                             </div>
                             <!-- form card login -->
                                 <div class="card card-outline-secondary w-75">
-                                    <div class="card-header py-3 text-center">
+                                    <div class="card-header py- text-center">
                                         <h4 class="mb-0 lead">Sign in to start your session</h4>
                                     </div>
                                     <div class="card-body p-5">
@@ -34,7 +48,7 @@
                                                     <label class="form-check-label" for="inlineFormCheck">Remember me</label>
                                                     <br>
                                                 </div>
-                                                    <p class="py-2"><a href="#">I Forget my password</a></p>
+                                                    <p class="py-1"><a href="#">I Forget my password</a></p>
                                                 <button type="submit" class="btn btn-info float-right">Sign in</button>
                                             </form>
                                             <br>
@@ -59,8 +73,8 @@
                                                                 <input type="radio" name="options" id="option3" autocomplete="off"> Purchase
                                                             </label>
                                                         </div>
-                                                        <table class="table table-bordered my-2">
-                                                                <thead class="">
+                                                        <table class="table table-bordered table-hover my-2">
+                                                                <thead class="bg-gray">
                                                                     <tr>
                                                                     <th scope="col">#</th>
                                                                     <th scope="col">Role</th>
@@ -71,21 +85,28 @@
                                                                 <tbody>
                                                                     <tr>
                                                                     <th scope="row">1</th>
-                                                                    <td>Mark</td>
-                                                                    <td>Otto</td>
-                                                                    <td>@mdo</td>
+                                                                    <td>Admin</td>
+                                                                    <td>admin</td>
+                                                                    <td>123456</td>
                                                                     </tr>
                                                                     <tr>
                                                                     <th scope="row">2</th>
-                                                                    <td>Jacob</td>
-                                                                    <td>Thornton</td>
-                                                                    <td>@fat</td>
+                                                                    <td>Sales</td>
+                                                                    <td>sales</td>
+                                                                    <td>123456</td>
                                                                     </tr>
                                                                     <tr>
                                                                     <th scope="row">3</th>
-                                                                    <td>Larry</td>
-                                                                    <td>the Bird</td>
-                                                                    <td>@twitter</td>
+                                                                    <td>Purchase</td>
+                                                                    <td>purchase</td>
+                                                                    <td>123456</td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                    <td colspan="4">
+                                                                        <p class="text-sm text-center">
+                                                                        Any number of Users & Roles you can create.
+                                                                        </p>
+                                                                        </td>
                                                                     </tr>
                                                                 </tbody>
                                                         </table>
@@ -123,6 +144,120 @@ section{
     justify-content: center;
     align-items: center;
     flex-direction: column;
+}
+.circles{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+
+.circles li{
+    position: absolute;
+    display: block;
+    list-style: none;
+    width: 20px;
+    height: 20px;
+    background: rgba(255, 255, 255, 0.2);
+    animation: animate 25s linear infinite;
+    bottom: -150px;
+    
+}
+
+.circles li:nth-child(1){
+    left: 25%;
+    width: 80px;
+    height: 80px;
+    animation-delay: 0s;
+}
+
+
+.circles li:nth-child(2){
+    left: 10%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 2s;
+    animation-duration: 12s;
+}
+
+.circles li:nth-child(3){
+    left: 70%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 4s;
+}
+
+.circles li:nth-child(4){
+    left: 40%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 0s;
+    animation-duration: 18s;
+}
+
+.circles li:nth-child(5){
+    left: 65%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 0s;
+}
+
+.circles li:nth-child(6){
+    left: 75%;
+    width: 110px;
+    height: 110px;
+    animation-delay: 3s;
+}
+
+.circles li:nth-child(7){
+    left: 35%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 7s;
+}
+
+.circles li:nth-child(8){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+
+.circles li:nth-child(9){
+    left: 20%;
+    width: 15px;
+    height: 15px;
+    animation-delay: 2s;
+    animation-duration: 35s;
+}
+
+.circles li:nth-child(10){
+    left: 85%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 0s;
+    animation-duration: 11s;
+}
+
+
+
+@keyframes animate {
+
+    0%{
+        transform: translateY(0) rotate(0deg);
+        opacity: 1;
+        border-radius: 0;
+    }
+
+    100%{
+        transform: translateY(-1000px) rotate(720deg);
+        opacity: 0;
+        border-radius: 50%;
+    }
+
 }
 </style>>
     
