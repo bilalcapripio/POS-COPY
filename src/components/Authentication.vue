62f20dd8fd251cc450e732b77ebf695ea232d5ef
bilal <bilal.rizwaan1909@gmail.com>
2021-03-09 12:57:49 +0500

    Authentication Page

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -13,26 +13,26 @@
                                         <h4 class="mb-0 lead">Sign in to start your session</h4>
                                     </div>
                                     <div class="card-body p-5">
-                                        <form>
-                                            <div class="form-group">
-                                                <input class="form-control"  required type="text" value="admin">
+                                        <form class="form-inline">
+                                            <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+                                            <div class="input-group mb-2 mr-sm-2">
+                                                <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
+                                                <div class="input-group-prepend">
+                                                <div class="input-group-text">@</div>
+                                                </div>
                                             </div>
 
-                                            <div class="form-group">
-                                                <input  class="form-control"  required type="password" value="admin">
-                                            </div>
+                                            
 
-                                            <div class="form-check small">
-                                                <label class="form-check-label">
-                                                    <input class="form-check-input" type="checkbox"> 
-                                                    <span>Remember me on this computer</span>
-                                                    <br>
-                                                    <span><a href="$">I Forget my password </a></span>
-
+                                            <div class="form-check mb-2 mr-sm-2">
+                                                <input class="form-check-input" type="checkbox" id="inlineFormCheck">
+                                                <label class="form-check-label" for="inlineFormCheck">
+                                                Remember me
                                                 </label>
                                             </div>
-                                            <button class="btn btn-info float-right">Login</button>
-                                        </form>
+
+                                            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                                            </form>
                                     </div>
                                     <!--/card-block-->
                                 </div>
